4c874fcce7fb199e451fb79f68fcb1f8ff7ffc43
Humberto <delacruz101@gmail.com>
2023-09-29 06:08:50 -0400

    uploading

--- Coordinator.py
+++ Coordinator.py
@@ -17,7 +17,7 @@ openai.api_key = OPENAI_API_KEY
 
 
 st.image("cumminslogo.png")
-st.header("FAQ Process Finder:")
+st.header("FAQ Cummins Finder:")
 
 question = st.text_input(
     "Please type below what you would like to learn more about and I will try to find the process for you to ask your questions. ", placeholder="Enter question...")
