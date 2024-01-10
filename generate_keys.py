9edb468cc5a5a57f73aa09cdbaeec6eb15b71c78
Humberto De La Cruz Santos <delacruz101@gmail.com>
2024-01-09 20:11:13 -0500

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 
 names = ["Humberto De La Cruz", "Peter Parker"]
-usernames = ["qd990", "pp990"]
+usernames = ["", ""]
 passwords = ["XXXXX", "XXXXX"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
